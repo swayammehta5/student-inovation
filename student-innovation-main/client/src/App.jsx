06ced6da900a10f3273fd3b94037bc9d3c15c5f1
VIVEK PACHNANDA <150216729+Hero9876s@users.noreply.github.com>
2026-02-27 12:25:07 +0530

    Update name from 'Vivek' to 'Vivek Pachnanda'
    Updated my name in the about section.

--- student-innovation-main/client/src/App.jsx
+++ student-innovation-main/client/src/App.jsx
@@ -312,7 +312,7 @@ function About() {
 
           <div className="about-card">
             <div className="avatar">TM</div>
-            <h3>Vivek</h3>
+            <h3>Vivek PAchnanda</h3>
             <p>Database / FrontEnd Contributor</p>
             <a href="#" target="_blank" rel="noreferrer">LinkedIn</a>
           </div>
